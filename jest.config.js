1fe1beb0848f5f83ab22ab43f47e10d486c9c915
Samuel Woodbridge <s-bridge@live.co.uk>
2022-04-03 21:47:04 +0100

    [Jest] - Config tweak

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,5 @@ const { compilerOptions } = require("./tsconfig.json");
 module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
-    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: "<rootDir>/" })
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: "<rootDir>" })
 };
